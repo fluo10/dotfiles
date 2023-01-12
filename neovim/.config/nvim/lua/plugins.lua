fee70fc134263642ebff132be0ea2bef9b38cbec
fluo10 <fluo10.dev@fireturtle.net>
2023-01-12 18:30:54 +0900

    Add nvim-treesitter-playground

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -25,7 +25,41 @@ return require('packer').startup(function(use)
         ft = 'csv'
     }
     --use "nvim-lua/plenary.nvim"
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { 
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+      end,
+      config = function()
+        require "nvim-treesitter.configs".setup {
+          highlight = {
+            enable = true,
+          },
+          playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+              toggle_query_editor = 'o',
+              toggle_hl_groups = 'i',
+              toggle_injected_languages = 't',
+              toggle_anonymous_nodes = 'a',
+              toggle_language_display = 'I',
+              focus_language = 'f',
+              unfocus_language = 'F',
+              update = 'R',
+              goto_node = '<cr>',
+              show_help = '?',
+            },
+          }
+        }
+      end
+    }
+    use { 
+      'nvim-treesitter/playground',
+      }
     use {
         "nvim-neorg/neorg",
         ft = "norg",
