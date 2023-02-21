da6bfcd36c5e402de3fe2eb162a796a3eee4558b
fluo10 <fluo10.dev@fireturtle.net>
2023-02-21 15:19:18 +0900

    Add function to activate neovimtree (#13)

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -30,3 +30,20 @@ vim.cmd([[
 
 require("nvim-web-devicons").get_icons()
 require("nvim-tree").setup()
+
+
+local function open_nvim_tree(data)
+
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  vim.cmd.cd(data.file)
+
+  require("nvim-tree.api").tree.open()
+
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
